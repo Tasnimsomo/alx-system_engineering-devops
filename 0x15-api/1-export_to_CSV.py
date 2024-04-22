ecd9304af3407040968481fbebab8cb90d7fa0b0
root <root@TasnimSomo>
2024-04-22 12:07:43 +0400

    0x15. API

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id)
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+    filename = "{}.csv".format(employee_id)
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in todos_data:
+            writer.writerow([task['userId'],
+                             user_data['username'],
+                task['completed'],
+                task['title']])
+
+    print(
+        "Tasks owned by employee {} have been saved to {}.".format(
+        user_data['username'],
+        filename))
