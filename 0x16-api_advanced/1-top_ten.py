3013868df0c49c1dc3918b6d691a41d74beee0d8
root <root@TasnimSomo>
2024-05-07 22:36:28 +0400

    0x16. API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,12 +7,13 @@ import requests
 
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
-    if response.status_code != 200:
-        print('None')
-        return
-
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64)"}
     try:
+        response = requests.get(url, allow_redirects=False)
+        if response.status_code != 200:
+            print('None')
+            return
+    
         posts = response.json()['data']['children']
         for post in posts:
             title = post['data']['title']
