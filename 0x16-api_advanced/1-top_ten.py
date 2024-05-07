45042fa0af72a23712afed4006be7ac70a7e36ea
root <root@TasnimSomo>
2024-05-07 22:38:47 +0400

    0x16. API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,9 +7,9 @@ import requests
 
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64)"}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     try:
-        response = requests.get(url, allow_redirects=False)
+        response = requests.get(url, allow_redirects=False, headers=headers)
         if response.status_code != 200:
             print('None')
             return
@@ -18,5 +18,5 @@ def top_ten(subreddit):
         for post in posts:
             title = post['data']['title']
             print(title)
-    except KeyError:
+    except Exception:
         print("None")
