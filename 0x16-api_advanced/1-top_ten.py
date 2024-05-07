7eb89f0fe3dee035fd88d4888451e861d5c7593d
root <root@TasnimSomo>
2024-05-07 12:08:46 +0400

    0x16. API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,9 +6,6 @@ import requests
 
 
 def top_ten(subreddit):
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, allow_redirects=False)
     if response.status_code != 200:
