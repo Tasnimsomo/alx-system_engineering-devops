fbde23231bed091cf6f1f6a2ab8ca9b86239a257
root <root@TasnimSomo>
2024-05-09 05:45:13 +0400

    0x17. Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+trace is your frienid
+# Fixes Apache 500 error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
